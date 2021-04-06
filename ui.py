ec36c3bb10681ce54cb6946faedebf1a6a8cf9f5
Menghao Yu <68170909+menghaoyu2002@users.noreply.github.com>
2021-04-06 00:25:30 -0400

    Implemented part of ResultsPage

--- ui.py
+++ ui.py
@@ -1,5 +1,5 @@
+from __future__ import annotations
 import tkinter as tk
-from tkinter import ttk
 from typing import Optional
 
 BG_COLOUR = 'gray10'
@@ -7,96 +7,210 @@ WINDOW_WIDTH = 900
 WINDOW_HEIGHT = 600
 
 class Page:
-    frame: ttk.Frame
-
     """ an abstract class representing a single page in the application"""
-    def __init__(self, frame) -> None:
-        self.frame = frame
+    _frame: tk.Tk
+    _user_interface: UserInterface
+
+    def __init__(self, frame: tk.Tk, user_interface: UserInterface) -> None:
+        self._frame = frame
+        self._user_interface = user_interface
 
     def show_page(self, truth: bool) -> None:
         """Make the current page visible or invisible based on truth
 
-        the page is visible in the main window if and only if truth == true
+        the page is visible if and only if truth == true
         """
         NotImplementedError
 
 
-class SearchField(Page):
-    """A class representing the search field"""
+class HomePage(Page):
+    """A class representing the Homepage of the application"""
+
+    # Private Instance Attributes:
     _search_variable: tk.StringVar
-    _frame: ttk.Frame
+    _frame: tk.Tk
     _text: Optional[tk.Label]
-    _field: Optional[tk.Entry]
+    _search_field: Optional[tk.Entry]
+    _submit_button: Optional[tk.Button]
+    _user_interface: UserInterface
+    _error_message: Optional[tk.Label]
 
-    def __init__(self, frame: tk.Tk, search_variable: tk.StringVar) -> None:
-        Page.__init__(self, frame)
+    def __init__(self, frame: tk.Tk, user_interface: UserInterface, search_variable: tk.StringVar) -> None:
+        """Initialize the homepage object"""
+        Page.__init__(self, frame, user_interface)
         self._search_variable = search_variable
-        self.show_page(True)
 
-    def place_text(self) -> None:
-        """Place "Search" label on the screen"""
-        self._text = tk.Label(self.frame, text='Search for a Song!', bg=BG_COLOUR, fg='gray95', font=('arialnarrow', 50))
+    def _place_text(self) -> None:
+        """Place the "Search" label on the screen"""
+        self._text = tk.Label(self._frame, text='Search for a Song!', bg=BG_COLOUR, fg='gray95', font=('arialnarrow', 50))
         self._text.pack()
+        self._text.place(x=(WINDOW_WIDTH) // 2, y=(WINDOW_HEIGHT) // 4, anchor='center')
 
-        self.frame.update()
-        text_height = self._text.winfo_height()
-        text_width = self._text.winfo_width()
-
-        self._text.place(x=(WINDOW_WIDTH - text_width)//2, y=(WINDOW_HEIGHT - text_height)//4)
-
-    def place_entryfield(self) -> None:
+    def _place_searchfield(self) -> None:
         """Dispaly the "Search" field on the screen"""
-        self._field = tk.Entry(self.frame, textvariable=self._search_variable, font=('Verdana', 50), bg='gray95')
-        self._field.pack()
+        self._search_field = tk.Entry(self._frame, textvariable=self._search_variable, font=('Verdana', 50), bg='gray95')
+        self._search_field.pack()
 
-        text_ypos = self._text.winfo_rooty()
-        self._field.place(x=(WINDOW_WIDTH)//2, y=((WINDOW_HEIGHT + text_ypos)//2.5),
-                         width=WINDOW_WIDTH * 0.75, height=100, anchor='center')
 
+        self._search_field.place(x=WINDOW_WIDTH // 2, y=(WINDOW_HEIGHT // 2),
+                          width=WINDOW_WIDTH * 0.75, height=100, anchor='center')
+
+    def _place_submit_button(self) -> None:
+        """Place the submit button on the user interface"""
+        self._submit_button = tk.Button(self._frame, text='Submit', command=self._search,
+                                       font=('arialnarrow', 20), bg='forest green', fg='white')
+        self._submit_button.pack()
+        self._submit_button.place(x=(WINDOW_WIDTH)//2, y=WINDOW_HEIGHT//1.25, anchor='center',
+                                  width=WINDOW_WIDTH//4, height=100)
+
+    def _show_error_message(self) -> None:
+        pass
+
+    def _search(self) -> None:
+        """Search for the song name represented by self._search_variable in the song graph"""
+        song_name = self._search_variable.get()
+        search_page = ResultsPage(self._frame, self._user_interface, self._search_variable, self)
+        self._user_interface.change_current_page(search_page)
+
+        # if song_name in graph:
+        #     pass # do something
+        # else:
+        #     pass
 
     def show_page(self, truth: bool) -> None:
         """Make the current page visible or invisible based on truth"""
         if truth:
-            self.place_text()
-            self.place_entryfield()
+            self._place_text()
+            self._place_searchfield()
+            self._place_submit_button()
         else:
             self._text.destroy()
-            self._field.destroy()
+            self._search_field.destroy()
+            self._submit_button.destroy()
+            self._error_message.destroy()
 
+
+class ResultsPage(Page):
+    """A page representing the search results page in the application from the give graph
+
+    This page is responsible for providing a graphical interface which displays the search results
+    """
+    # Private Instance Attributes:
+    _frame: tk.Tk
+    _user_interface: UserInterface
+    _search_variable: tk.StringVar
+    _submit_button: Optional[tk.Button]
+    _home_button: Optional[tk.Button]
+    _home_page: HomePage
+    _search_results: SearchResults
+    _error_message: Optional[tk.Label]
+
+    def __init__(self, frame: tk.Tk, user_interface: UserInterface, search_variable: tk.StringVar, home_page: HomePage) -> None:
+        """Initialize the Search results page"""
+        Page.__init__(self, frame, user_interface)
+        self._search_variable = search_variable
+        self._home_page = home_page
+
+    def _place_searchfield(self):
+        """Display a search field on the current page"""
+        self._search_field = tk.Entry(self._frame, textvariable=self._search_variable, font=('Verdana', 20), bg='gray95')
+        self._search_field.pack()
+
+
+        self._search_field.place(x=WINDOW_WIDTH // 2, y=(WINDOW_HEIGHT // 15),
+                          width=WINDOW_WIDTH * 0.75, height=50, anchor='center')
+
+    def _place_submit_button(self) -> None:
+        """Display the submit button on the user interface"""
+        self._submit_button = tk.Button(self._frame, text='Submit', command=self._search,
+                                       font=('arialnarrow', 15), bg='forest green', fg='white')
+        self._submit_button.pack()
+        self._submit_button.place(x=WINDOW_WIDTH - 10, y=(WINDOW_HEIGHT //15),
+                                  width=WINDOW_WIDTH//10, height=50, anchor='e',)
+
+    def _place_home_button(self):
+        """Display the home button on the current page"""
+        self._home_button = tk.Button(self._frame,
+                                      text='Home',
+                                      command= lambda: self._user_interface.change_current_page(self._home_page),
+                                      font=('arialnarrow', 15),
+                                      bg='gray60',
+                                      fg='white')
+
+        self._home_button.pack()
+
+        self._home_button.place(x=10, y=(WINDOW_HEIGHT //15),
+                                  width=WINDOW_WIDTH//10, height=50, anchor='w',)
+
+    def _show_error_message(self, truth: bool) -> None:
+        pass
+
+    def _show_results(self):
+        self._search_results = SearchResults()
+
+    def _search(self) -> None:
+        """Search for the song name represented by self._search_variable in the song graph"""
+        song_name = self._search_variable.get()
+        print(song_name)
+
+        # if song_name in graph:
+        #     pass # do something
+        # else:
+        #     pass
+
+    def show_page(self, truth: bool) -> None:
+        if truth:
+            self._place_searchfield()
+            self._place_submit_button()
+            self._place_home_button()
+            self._show_results()
+        else:
+            self._search_field.destroy()
+            self._submit_button.destroy()
+            self._home_button.destroy()
+            self._error_message.destroy()
+
+
+class SongRecommendations(Page):
+    """A class respresenting a page which will display the song recommendations from graph"""
+    _prev_page: ResultsPage
+    _home_page: HomePage
+
+    def __init__(self, frame: tk.Tk, user_interface: UserInterface, graph, prev_page: ResultsPage, home_page: HomePage) -> None:
+        Page.__init__(self, frame, user_interface)
+        self._prev_page = prev_page
+        self._home_page = home_page
+
+
+class SearchResults:
+    pass
 
 class UserInterface:
-    """A class representing the user interface"""
+    """A class representing the main user interface"""
     mainframe: tk.Tk
     search_variable: tk.StringVar
-    search_field: SearchField
-    submit_button: tk.Button
     graph: ...
+
+    _current_page: Optional[Page]
 
     def __init__(self, root: tk.Tk, graph=None) -> None:
         """Initialize the user interface"""
         root.title('Spotify Song Recommender')
         self.mainframe = root
 
-        # Placing the search field
+        # Initializing the home page
         self.search_variable = tk.StringVar()
-        self.search_field = SearchField(self.mainframe, self.search_variable)
-        self.place_submit_button()
+        self._current_page = HomePage(self.mainframe, self, self.search_variable)
+        self._current_page.show_page(True)
 
-    def place_submit_button(self) -> None:
-        self.submit_button = tk.Button(self.mainframe, text='Submit', command=self.search,
-                                       font=('arialnarrow', 20), bg='forest green', fg='white')
-        self.submit_button.pack()
-        self.submit_button.place(x=(WINDOW_WIDTH)//2, y=WINDOW_HEIGHT//1.25, anchor='center',
-                                 width=WINDOW_WIDTH//4, height=100)
+    def change_current_page(self, page: Page) -> None:
+        """Change the current page to <page> and display it"""
+        # make the current page invisible
+        self._current_page.show_page(False)
 
-    def search(self) -> None:
-        """Search for the song name represented by self._search_variable in the song graph"""
-        song_name = self.search_variable.get()
-        print(song_name)
-        # if song_name in graph:
-        #     pass # do something
-        # else:
-        #     pass
+        # update the current page and make it visible
+        self._current_page = page
+        self._current_page.show_page(True)
 
 
 
