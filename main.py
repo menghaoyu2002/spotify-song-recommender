ed6280279dfab92afeb580e4a10edce9c9c902fd
Menghao <menghaoyu2002@gmail.com>
2021-04-16 12:13:02 -0400

    Added main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+from ui import UserInterface, WINDOW_HEIGHT, WINDOW_WIDTH, BG_COLOUR
+import tkinter as tk
+from graphs import build_graph
+
+
+def run_program() -> None:
+    """Run the program"""
+    root = tk.Tk()
+    root.configure(background=BG_COLOUR)
+
+    root.resizable(width=False, height=False)
+    root.geometry(f'{WINDOW_WIDTH}x{WINDOW_HEIGHT}')
+
+    # graph = build_graph('data/data_large.csv')
+    graph = build_graph('data/data_small.csv')
+
+    UserInterface(root, graph)
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    run_program()
